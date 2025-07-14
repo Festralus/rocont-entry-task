d5e3a1ac7740650d08f49ec377d19bdcf3c2d4bc
Festralus <kezarnus@gmail.com>
2025-07-14 06:15:33 +0300

    feat: GH pages host

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/rocont-entry-task/",
+  // base: "/rocont-entry-task/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
